af34d26667d34313f2e722cb70e174eee19e0fce
Kunal Rathod <36470138+KunalRRathod@users.noreply.github.com>
2020-03-13 23:35:30 +0530

    Assignment Question_5

---
+++ CustomTag_Gen_by_SAX_Parser[Question_5]/src/com/cognizant/TagHandler.java
@@ -0,0 +1,34 @@
+package com.cognizant;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+// Create Tag Handler
+public class TagHandler extends TagSupport {
+	@Override
+	public int doStartTag() throws JspException {
+
+		System.out.println("from doStartTag()");
+
+		JspWriter out = pageContext.getOut();
+		try {
+			out.write("<h1>" + new Date() + "</h1>");
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		return EVAL_BODY_INCLUDE;
+	}
+
+	@Override
+	public int doEndTag() throws JspException {
+
+		System.out.println("from doEndTag()");
+		return EVAL_PAGE;
+	}
+
+}
